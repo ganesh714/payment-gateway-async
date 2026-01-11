753256418642b54f56840000160ba73667df47cf
ganesh714 <evvganesh1@gmail.com>
2026-01-11 21:42:06 +0530

    fix(build): Fix checkout-page config for ESM and install missing deps

--- checkout-page/webpack.config.js
+++ checkout-page/webpack.config.js
@@ -1,13 +1,19 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: './src/sdk/PaymentGateway.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'checkout.js',
-    library: 'PaymentGateway',
-    libraryTarget: 'umd',
-    libraryExport: 'default', // Correctly export the class as the default
+    library: {
+      name: 'PaymentGateway',
+      type: 'umd',
+      export: 'default',
+    },
     globalObject: 'this'
   },
   module: {
@@ -22,7 +28,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-             presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
